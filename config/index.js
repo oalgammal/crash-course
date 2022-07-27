0c3b311485e4e0cefeaa63428a44f71d5a42784b
oalgammal <oalgammal@gmail.com>
2022-07-27 17:24:47 +0200

    config edited 4

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
 const dev = process.env.NODE_ENV !== 'production'
-export const server = dev? 'http://localhost:3000':'https://next-ivory-chi.vercel.app/'
+export const server = dev? 'http://localhost:3000':'http://localhost:3000'
