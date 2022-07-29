bd65a4df0e54dbacb87fd4e52f25b6819f869bf0
oalgammal <oalgammal@gmail.com>
2022-07-29 03:44:44 +0200

    lets try lower case2

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,16 +1,16 @@
 import articleStyles from '../styles/Article.module.css'
-import link from 'next/Link'
+import Link from 'next/link'
 
 const ArticleItem= ({article})=>{
 	return(
-		<link href="/article/[id]" as={`/article/${article.id}`}>
+		<Link href="/article/[id]" as={`/article/${article.id}`}>
 			
 	        	<a className={articleStyles.card}>
 	        		<h3>{article.title} &rarr;</h3>
 	        		<p>{article.excerpt}</p>
 	        	</a>
 	      	
-		</link>
+		</Link>
 		)
 }
 
