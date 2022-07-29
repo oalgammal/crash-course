a96bfee9fc93110c44c975716e0cb57468ad20de
oalgammal <oalgammal@gmail.com>
2022-07-29 05:02:52 +0200

    user agent2

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -39,7 +39,8 @@ export const getStaticPaths = async ()=>{
         }, //this is required by api provider
         })
 	const articles = await res.json()
-	const ids = articles.map(article=>article.id)
+	console.log(articles)
+	const ids = articles.articles.map(article=>article.id)
 	const paths = ids.map(id=>({params:{id:id.toString()}}))
 
 	return{ 
